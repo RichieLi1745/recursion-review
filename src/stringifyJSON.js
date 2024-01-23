17a0a9a9a7611fa01d57180c735e94a0dc1d65c6
Richard Li <richardli1745@gmail.com>
2024-01-22 19:39:37 -0800

    update my Sprint Recursion Pair Review

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,67 @@
 
 // but you don't so you're going to write it from scratch:
 
+//Input: primitive values, arrays, objects, functions, (symbols)
+//Output: string (in JSON format)
+//Constraints: don't cheat
+//Edge cases: undefined, empty object, empty array, single values; functions; undefined in an array (null); undefined in an object (ommitted); infinity, NaN (both null);
+//
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var resultString = '';
+  //handle the edge cases
+  if (obj === undefined) {
+    return undefined;
+  }
+  if (obj === null || typeof(obj) === 'function' || obj === Infinity || obj === NaN) {
+    return 'null';
+  }
+  if (obj === '') {
+    return '""';
+  }
+  //handle primitve values
+  if (typeof(obj) === 'string') {
+    resultString += '"' + obj + '"';
+  }
+  if (typeof(obj) === 'boolean') {
+    return String(obj);
+  }
+  if (typeof(obj) === 'number') {
+    return String(obj);
+  }
+
+
+  //handle arrays
+  if (Array.isArray(obj)) {
+    resultString += '[';
+
+    obj.forEach(function(item, index, array) {
+      if (item === undefined) {
+        resultString += 'null';
+      } else {
+        resultString = resultString + stringifyJSON(item);
+      }
+      if (array.length - 1 !== index) {
+        resultString += ',';
+      }
+    });
+
+    resultString += ']';
+  //handle objects
+  } else if (typeof(obj) === 'object') {
+    resultString += '{';
+    var keys = Object.keys(obj);
+    keys.forEach(function(key, index, array) {
+      var value = obj[key];
+      if (typeof(value) !== 'function' && value !== undefined) {
+        resultString = resultString + '"' + String(key) + '":' + stringifyJSON(value);
+        if (array.length - 1 !== index) {
+          resultString += ',';
+        }
+      }
+    });
+    resultString += '}';
+  }
+
+  return resultString;
+
 };
